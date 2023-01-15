96077575b3dffeb8013a7b4ecbc4ae1990454789
SohamDesai1 <sohamdesai123@gmail.com>
2023-01-15 13:57:16 +0530

    writing the onchain code

---
+++ src/instruction.rs
@@ -0,0 +1,32 @@
+use solana_program::{ program_error::ProgramError};
+
+#[derive(Debug)]
+pub enum HelloInstruction {
+    Increment,
+    Decrement,
+    SetCounter(u32),
+}
+
+impl HelloInstruction {
+    pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
+        let (&tag, rest) = input.split_first().ok_or(ProgramError::InvalidInstructionData)?;
+        match tag {
+            0 => Ok(HelloInstruction::Increment),
+            1 => Ok(HelloInstruction::Decrement),
+            2 => {
+                if rest.len() != 4 {
+                    return Err(ProgramError::InvalidInstructionData);
+                }
+                let val: Result<[u8; 4], _> = rest[..4].try_into();
+                match val {
+                   Ok(i)=>{
+                        return Ok(HelloInstruction::SetCounter(u32::from_le_bytes(i)));
+                   }
+                    _ =>   return Err(ProgramError::InvalidInstructionData)
+                    
+                }
+            },
+            _ => Err(ProgramError::InvalidInstructionData),
+        }
+    }
+}
